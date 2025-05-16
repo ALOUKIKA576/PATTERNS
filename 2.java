665bbec7b24b022c733fc4e5f8cc8f75b7e1400e
ALOUKIKA576 <aloukikakottapalli@gmail.com>
2025-05-16 11:35:05 +0530

    Update 2.java

--- 2.java
+++ 2.java
@@ -10,6 +10,20 @@ public class Solution {
         }
     }
 }
+or
+public class Solution {
+    public static void nForest(int n) {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 
 
 //output
